4ea06ee93839875fbd45831700ecbbb08c20513f
asyasahin <asyasahin@example.com>
2022-10-11 16:59:26 +0300

    Models/TodoListItem modified

--- Models/TodoListItem.cs
+++ Models/TodoListItem.cs
@@ -2,5 +2,11 @@
 {
     public class TodoListItem
     {
+        public int Id { get; set; } 
+
+        public string? work { get; set; }
+
+        public bool IsComplete { get; set; }    
+
     }
 }
